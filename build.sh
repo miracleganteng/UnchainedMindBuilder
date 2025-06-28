8dbe8cba9fb66bd02c656701a1a14125c9dbfefa
miracleganteng <kumisajaib54@gmail.com>
2025-06-28 16:47:29 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,4 @@
+echo "[🔧] Mempersiapkan folder output..."
+mkdir -p output/assets/www
+cp index.html script.js config.js style.css manifest.json service-worker.js output/assets/www/
+echo "[✅] Semua file siap di folder: output/assets/www/"
